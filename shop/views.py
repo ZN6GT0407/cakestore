05df422fc380302a233dadcebe4d7c4a46c409d2
ZN6GT0407 <149847060+ZN6GT0407@users.noreply.github.com>
2024-06-23 16:04:52 +0800

    Add files via upload

---
+++ shop/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Cake, Order, Profile
+from .forms import ProfileForm, OrderForm
+import logging
+
+logger = logging.getLogger(__name__)
+
+def index(request):
+    cakes = Cake.objects.all()
+    return render(request, 'shop/index.html', {'cakes': cakes})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+        profile_form = ProfileForm()
+    return render(request, 'shop/register.html', {'form': form, 'profile_form': profile_form})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('index')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'shop/login.html', {'form': form})
+
+@login_required
+def order(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
+            return redirect('order_confirmaton', order_id=order.id)
+        
+    else:
+        form = OrderForm()
+    return render(request, 'shop/order.html', {'form': form})
